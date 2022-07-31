e82c3cdfe9ba70b49c455603cc3ed8a75a28a0cc
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-07-31 16:02:32 +0300

    Finish backend part: add middlewares and authentication

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
 //Главный файл программы, тут устанавливаются базовые параметры: порт, требование экспресса и отправная точка
-require('dotenv').config();
-const express = require('express');
-const models = require('./models/models');
-const cors = require('cors');
-const sequelize = require('./db');
+require('dotenv').config();// database
+const express = require('express');// express import
+const models = require('./models/models');//модели сущностей
+const cors = require('cors');//
+const sequelize = require('./db');//взаимодействие с базой данных
+const router = require('./routes/index');//роутинг
+const fileUpload = require('express-fileupload');//выгрузка файлов
+const errorHandler = require('./middleware/errorHandlingMiddleware');//обработка ошибок
+const path = require('path');
+const PORT = process.env.PORT || 5000;//установка порта
+const app = express();//создание сервера
+app.use(cors());//запросы
+app.use(express.json());//работа с json
+app.use(express.static(path.resolve(__dirname, 'static')));//работа с static страницами
+app.use(fileUpload({}));
+app.use('/api', router);//роутинг
 
 
-
-const PORT = process.env.PORT || 5000;
-const app = express();
-app.use(cors());
-app.use(express.json());
+//замыкающий
+app.use(errorHandler);//обработчик ошибок
 
 const start = async () => { //попытка подключения к БД с сервером
 
---
+++ server/routes/typeRouter.js
@@ -0,0 +1,8 @@
+const Router = require("express");
+const router = new Router();
+const typeController = require('../controllers/typeController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
+router.post('/',checkRoleMiddleware, typeController.create);
+router.get('/',typeController.getAll);
+
+module.exports = router;
